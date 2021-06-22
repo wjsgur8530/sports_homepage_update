c2cdf0e629b740dc49945f19115bc6c03c7367df
wjsgur8530 <wjsgur8530@gmail.com>
2021-06-22 12:37:45 +0900

    'sportshomepage static'

--- homepageProject/settings.py
+++ homepageProject/settings.py
@@ -40,7 +40,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
